0647138fd4064eb3fd6cc7bdb5eceb16e31cacc0
Winnona Sarah <winnona.sarah@gmail.com>
2022-03-02 13:36:12 +0700

    add assertion test to send method

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @Test
-    @DisplayName("Show user list")
+    @DisplayName("Send points test")
     void send() {
         ArrayList<User> users = new ArrayList<User>();
         users.add(new User("Eva", 0, 500));
@@ -21,7 +21,15 @@ class MainTest {
         String fail = "Transaction failed.";
 
         assertEquals(success, Main.sendPoints(users.get(0), users.get(1),500));
+        assertEquals(0, users.get(0).getPoints()); //Eva: 0
+        assertEquals(1000, users.get(1).getPoints()); //Sarah: 1000
+
         assertEquals(success, Main.sendPoints(users.get(1), users.get(2),700));
+        assertEquals(300, users.get(1).getPoints()); //Sarah: 300
+        assertEquals(1200, users.get(2).getPoints()); // Rahma: 1200
+
         assertEquals(fail, Main.sendPoints(users.get(0), users.get(2),100));
+        assertEquals(0, users.get(0).getPoints()); //Eva: 0 karna send fail
+        assertEquals(1200, users.get(2).getPoints()); // Rahma: 1200 ga nambah
     }
 }
